fcce129444b7854032d7d481824a24f5ce6ab8fe
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-06-29 12:06:49 +0530

    Add palindrome checking logic in C
    Fix syntax errors and implement palindrome check.

---
+++ pallindrome problem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int n,rev=0,rem,temp;
+  scanf("%d",&n):
+    temp=n;
+while(n!=0)
+  rem=n%10:
+  rev=rev*10+rem;
+  n=n/10;
+
+if(rev==temp)
+{
+  printf("palindrome");
+}
+else
+{
+printf("not palindrome"):
+
+  return 0;
+}
+  
